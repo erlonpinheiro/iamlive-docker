8c9fd738f18d9481c70af19c46dc001431082e99
Erlon Sousa Pinheiro <erlons@gmail.com>
2025-04-09 09:44:05 -0700

    Modify entrypoint to prioritize arguments from docker compose.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,21 @@ set -o pipefail
 _FORMAT_LOGS="${FORMAT_LOGS:-"true"}"
 _ALLOWED_ADDRESS="${ALLOWED_ADDRESS:-"0.0.0.0"}"
 
-if [[ "$_FORMAT_LOGS" = "true" ]]; then
-    /app/iamlive --output-file "/app/iamlive.log" \
-        --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@ | jq -c .
+if [[ "$#" -gt 0 ]]; then
+    echo "[INFO] Running iamlive with customized arguments: $@"
+    exec /app/iamlive "$@"
 else
-    /app/iamlive --output-file "/app/iamlive.log" \
-        --mode proxy --bind-addr "${_ALLOWED_ADDRESS}:10080" $@
+    echo "[INFO] Running iamlive with default arguments."
+    if [[ "$_FORMAT_LOGS" = "true" ]]; then
+        exec /app/iamlive \
+            --output-file "/app/iamlive.log" \
+            --mode proxy \
+            --bind-addr "${_ALLOWED_ADDRESS}:10080" \
+            | jq -c .
+    else
+        exec /app/iamlive \
+            --output-file "/app/iamlive.log" \
+            --mode proxy \
+            --bind-addr "${_ALLOWED_ADDRESS}:10080"
+    fi
 fi
